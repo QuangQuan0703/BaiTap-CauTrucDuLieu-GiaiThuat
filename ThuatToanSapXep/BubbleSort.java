c9441b138f21baac457b8370fc29b5cd6b942f59
Quang Quan <nguyenquangquan_t65@hus.edu.vn>
2022-11-11 10:42:25 +0700

    Lần đầu commit gồm code phần sắp xếp

---
+++ ThuatToanSapXep/BubbleSort.java
@@ -0,0 +1,28 @@
+package ThuatToanSapXep;
+
+public class BubbleSort {
+    int [] arr;
+    public BubbleSort(int [] arr){
+        this.arr = arr;
+    }
+    public void print(){
+        System.out.println("Bubble Sort");
+        for (int i =0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public void Sort (){
+        int length = this.arr.length;
+        int temp;
+        for (int i =0; i < length -1; i++){
+            for (int j = 0; j< length - i - 1; j++){
+                if (arr[j] > arr[j + 1]){
+                    temp = arr[j];
+                    arr[j]= arr[j + 1];
+                    arr[j +1] = temp;
+                }
+            }
+        }
+        print();
+    }
+}
---
+++ ThuatToanSapXep/Main.java
@@ -0,0 +1,9 @@
+package ThuatToanSapXep;
+
+public class Main {
+    public static void main(String[] args) {
+        int [] arr = {4,2,5,44,3,1,6,7,4,12,986,4,523};
+        BubbleSort bubbleSort = new BubbleSort(arr);
+        bubbleSort.Sort();
+    }
+}
